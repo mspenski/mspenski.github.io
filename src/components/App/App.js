586848c373894e570dd523dbcf69d9d2a3b44e56
mspenski <44811974+mspenski@users.noreply.github.com>
2019-03-18 21:56:33 -0400

    react portfolio upload

---
+++ src/components/App/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Home from '../../pages/Home/Home';
+import Contact from '../../pages/Contact/Contact'
+import Portfolio from '../../pages/Portfolio/Portfolio'
+import NotFound from '../../pages/NotFound/NotFound'
+import Nav from '../Nav/Nav'
+import Footer from '../Footer/Footer'
+
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Nav />
+
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/portfolio' component={Portfolio} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
